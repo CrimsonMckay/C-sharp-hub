691fe8166ef3111d5de35ae2fa2a6b7a7b24cb8b
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2024-02-14 15:59:55 +0000

    Void methods don't return values

---
+++ void_methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace void_methods
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           int result= addition(2, 4);
+            Console.WriteLine(result);  
+            PrintToScreen();
+
+
+        }
+
+       static public int addition(int x,int y) { 
+            //non void
+            return x+y; 
+        
+        }
+
+      static void PrintToScreen()
+        {
+            //void method
+            Console.WriteLine("Hello There");
+            Console.ReadLine();
+        }
+    }
+}
