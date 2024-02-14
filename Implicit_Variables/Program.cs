4a736f543a94ea3945d8a43fc5df850f4bc99112
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2024-02-14 13:47:40 +0000

    The C# var keyword is used to create implicit typed local variable

---
+++ Implicit_Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = 20;
+            var s = "C sharp";
+            var b= 'D';
+
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+
+        }
+    }
+}
