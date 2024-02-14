0c0b2cc37491d7adc1a5bf8f734d037ec79c38c2
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2024-02-14 15:20:52 +0000

    If-else statement is used to test Condition

---
+++ conditional_statement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditional_statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+
+            if (Int32.Parse(age) < 18)
+            {
+                Console.WriteLine("You're not eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("Thanks for voting");
+
+            }
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
