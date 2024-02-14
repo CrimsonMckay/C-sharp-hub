9bd69127ddb17c0013db81b51850cd30e2960e5e
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2024-02-14 13:57:40 +0000

    The ReadLine() method is a commonly used method or function to take an input from the user

---
+++ User_Input/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("what is your name");
+
+            name=Console.ReadLine();
+
+            Console.WriteLine("Hi "+ name + " Welcome Welcome to the VR game,Shangri-la!!");
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
